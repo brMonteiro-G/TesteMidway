07f0b142640eb0dfb793880c269f7f0dff2f3d6c
brMonteiro-G <brgabriel.monteiro@gmail.com>
2022-03-07 01:50:03 -0300

    customizando mensagens de erro com js

--- js/main.js
+++ js/main.js
@@ -1,134 +1,100 @@
 
 
-function validarEmail() {
-    var email = document.querySelector('#inputEmail');
-    var error = document.querySelector('#error-email');
+//procura todas as tags que contém required e coloca em fields 
+const fields = document.querySelectorAll("[required]")
+// console.log(fields)
 
-    if (!email.checkValidity()) {
-        error.innerHTML = "Email invalido";
+
+function validateField(field) {
+    // lógica para verificar se existem erros 
+    function verifyErrors() {
+
+        let foundError = false;
+
+        for (let error in field.validity) {
+            if (field.validity[error] && !field.validity.valid) {
+                foundError = error
+            }
+        }
+
+        console.log(foundError)
+        return foundError;
     }
+
+    function customMessage(typeError){
+        const messages = {
+            text:{
+                valueMissing:"Por favor, preencha este campo",
+                patternMismatch: "Formato inválido"
+            },
+            email:{
+                valueMissing: "Email é obrigatório",
+                typeMismatch: "Por favor, insira um email válido"
+            },
+            
+        }
+        return messages[field.type][typeError]
+    }
+
+    function setCustomMessage(message) {
+        const spanError = field.parentNode.querySelector("span.error")
+        if (message) {
+            spanError.classList.add("active")
+            spanError.innerHTML = message
+        } else {
+
+            spanError.classList.remove("active")
+            spanError.innerHTML = ""
+        }
+    }
+
+    return function () {
+
+        const error = verifyErrors()
+        if (verifyErrors()) {
+            const message = customMessage(error)
+            field.style.borderColor="red"
+            setCustomMessage(message)
+        } else {
+            field.style.borderColor="green"
+            setCustomMessage()
+        }
+    }
+}
+
+function customValidation(event) {
+
+
+
+    const field = event.target
+    //validation se torna uma função anônima 
+    const validation = validateField(field)
+
+
+    validation()
+
+    // const error = validateField(field)
+    // console.log("Error exists: ", error)
+
 
 }
 
-function msgEmail() {
-    var error = document.querySelector('#error-email');
-    if (error.innerHTML == "Email invalido") {
-        error.innerHTML = "";
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-// const fields = document.querySelectorAll("[required]")
-// //console.log(fields)
-
-
-
-
-
-// function customValidation(event) {
-//     const field = event.target
-
-//     //eliminar bubble
-//     event.preventDefault()
-
-//     //lgc para verificar se existem erros
-//     function verifyErrors() {
-//         let foundError = false;
-
-//         for (let error in field.validity) {
-//             //se n for custom error então verifica se tem erro 
-//             if ( field.validity[error]) {
-//                 foundError = error
-//             }
-//         }
-
-//         return foundError;
-//     }
-
-//     const error = verifyErrors()
-//     console.log("Error exists:", error)
-
-//     const spanError = field.parentNode.querySelector("#error-email")
-
-//   if(error){
-    
-//     spanError.classList.add("active")
-//     spanError.innerHTML = "Campo inválido"
-
-//   }else{
-//     spanError.classList.remove("active")
-//     spanError.innerHTML = ""
-//   }
-
-// }
-
-// for (field of fields) {
-//     field.addEventListener("invalid", customValidation)
-// }
-
-
-
-//Mensagem personalizada
-  // if (error) {
-    //     //trocar msg de required
-    //     field.setCustomValidity("Esse campo é obrigatório")
-
-    // } else {
-    //     field.setCustomValidity("")
-    // }
-
-
-    document.querySelector("form")
-    .addEventListener("submit", event => {
-        console.log("enviar formulário")
-    })
-
-    
- function submit(){
-
-       console.log("está funcionando")
-
-
-       
-       document.getElementById('form-nome').submit();
-       document.getElementById('form-cartao').submit();
-
-       
-     }
-
-
-
-
-
-
-const fields =   document.querySelectorAll("[required]")
-
-console.log(fields)
-
-for(field of fields){
+for (let field of fields) {
     field.addEventListener("invalid", event => {
-        console.log("Campo inválido")
+
+        //eliminar o bubble
+        event.preventDefault()
+        customValidation(event)
     })
+    field.addEventListener("blur", customValidation)
+
 
 }
 
 
+document.querySelector("form").addEventListener("submit", event => {
+    console.log("enviar formulário")
 
-
-
-
-
-    
-
-
-    
+    //não vai enviar o formulário
+    event.preventDefault()
+})
